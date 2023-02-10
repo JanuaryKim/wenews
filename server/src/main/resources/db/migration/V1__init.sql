48b2de8bf30ae00b6b212082c8a0b0b0922e9c8e
JanuaryKim <unool6976@gmail.com>
2023-02-10 17:13:12 +0900

    feat : flyway 연동
    - news, member 테이블 생성

---
+++ server/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS `wenews`.`member`(
+
+    `member_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `member_nickname` VARCHAR(10),
+    `member_password` VARCHAR(12),
+    `member_email` VARCHAR(100),
+    `member_age` TINYINT,
+    PRIMARY KEY (`member_id`));
+
+
+CREATE TABLE IF NOT EXISTS `wenews`.`news`(
+
+    `news_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `member_id` BIGINT,
+    `news_title` VARCHAR(20),
+    `news_content` TEXT,
+    `news_tag` VARCHAR(100),
+    `created_at` DATETIME,
+    `modified_at` DATETIME,
+    PRIMARY KEY (`news_id`));
