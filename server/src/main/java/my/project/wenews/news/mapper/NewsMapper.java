5f4762758f602fb4d1a88ed24262e1f4bb4bc68c
JanuaryKim <unool6976@gmail.com>
2023-02-16 23:24:42 +0900

    feat : NewsMapper 수정
    - newsDtoPostToNews 메소드 추가
    newsTags는 코드 자동생성에서 제외 시킴
    - newsTags는 default 키워드 사용하여 newsTagArrToNewsTagStr 메소드로 매핑

--- server/src/main/java/my/project/wenews/news/mapper/NewsMapper.java
+++ server/src/main/java/my/project/wenews/news/mapper/NewsMapper.java
@@ -1,7 +1,39 @@
 package my.project.wenews.news.mapper;
 
+import my.project.wenews.news.dto.NewsDto;
+import my.project.wenews.news.entity.News;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Arrays;
 
 @Mapper(componentModel = "spring")
-public class NewsMapper {
+public interface NewsMapper {
+
+
+    @Mapping(target = "newsTags", ignore = true) //매핑할 source인 Post에서 newsTags는 제외 시킴
+    News newsDtoPostToNews(NewsDto.Post post);
+
+
+    //매핑할 로직을 직접 입력해야 하므로 default로 정의
+    default News newsTagArrToNewsTagStr(News news, NewsDto.Post post) {
+        String[] newsTags = post.getNewsTags();
+        Arrays.sort(newsTags);
+
+        if(newsTags.length == 0)
+            return news;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|");
+
+        for (String tag : newsTags) {
+            sb.append(tag);
+            sb.append("|");
+        }
+
+        news.setNewsTags(sb.toString());
+        return news;
+    }
+
 }
