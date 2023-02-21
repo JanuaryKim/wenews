3930a34a03f2bc01fc00254ba1fb72b778037f59
JanuaryKim <unool6976@gmail.com>
2023-02-21 21:56:31 +0900

    feat : MemberStub 수정
    - stub 엔티티 데이터 생성

--- server/src/test/java/my/project/wenews/member/stub/MemberStub.java
+++ server/src/test/java/my/project/wenews/member/stub/MemberStub.java
@@ -1,10 +1,25 @@
 package my.project.wenews.member.stub;
 
+import my.project.wenews.member.entity.Member;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MemberStub {
 
-    public static Map<String,Object> memberMap = new HashMap<>();
+    public static Map<String,Object> memberDtoMap = new HashMap<>();
+    public static List<Member> memberList;
 
+    static {
+
+        Member member = new Member();
+        member.setMemberId(1L);
+        member.setMemberNickname("kim");
+        member.setMemberEmail("wot00@naver.com");
+        member.setMemberPassword("guerero");
+
+        memberList = List.of(member);
+    }
 }
