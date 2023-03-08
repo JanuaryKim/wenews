50bef038800d44c52215dade18ba9d249523f3b6
JanuaryKim <unool6976@gmail.com>
2023-03-09 08:59:18 +0900

    feat : Oauth2 컴포넌트에 @Component 추가, Security설정 추가

--- server/src/main/java/my/project/wenews/config/auth/SecurityConfig.java
+++ server/src/main/java/my/project/wenews/config/auth/SecurityConfig.java
@@ -1,16 +1,18 @@
 package my.project.wenews.config.auth;
 
-
 import lombok.RequiredArgsConstructor;
 import my.project.wenews.oauth2.Oauth2Service;
 import my.project.wenews.oauth2.Oauth2SuccessHandler;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @RequiredArgsConstructor
+@Configuration
 public class SecurityConfig {
 
     private final Oauth2SuccessHandler oauth2SuccessHandler;
@@ -29,20 +31,26 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .apply(new CustomFilterConfigurer())
                 .and()
-                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .authorizeRequests().antMatchers("/api/auth/**").hasRole("USER")
+                .anyRequest().permitAll()
+                .and()
                 .oauth2Login()
                 .successHandler(oauth2SuccessHandler)
-                .userInfoEndpoint()
-                .userService(oauth2Service);
-
+                .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정을 저장
+                .userService(oauth2Service); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록
         return http.build();
+
+
+
+
     }
 
 
     private class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
         @Override
         public void configure(HttpSecurity builder) throws Exception {
-            super.configure(builder);
+//            super.configure(builder);
         }
     }
+
 }
--- server/src/main/java/my/project/wenews/oauth2/Oauth2SuccessHandler.java
+++ server/src/main/java/my/project/wenews/oauth2/Oauth2SuccessHandler.java
@@ -2,12 +2,14 @@ package my.project.wenews.oauth2;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
