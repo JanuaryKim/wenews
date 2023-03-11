0b899f1c40eda8a6c6092ea055f3decf2ce3b8b6
JanuaryKim <unool6976@gmail.com>
2023-03-11 22:51:36 +0900

    feat : Oauth2 기본 인증과 인가상태에 따른 버튼 생성 완성
    - 기본적인 Oauth2 작업 완료
    - 인증 방식 선정 전

--- server/src/main/java/my/project/wenews/page/IndexController.java
+++ server/src/main/java/my/project/wenews/page/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,14 +22,16 @@ public class IndexController {
     private final NewsMapper newsMapper;
 
     @GetMapping(value = "/")
-    public String index(Model model) {
+    public String index(Model model, Principal principal) {
 
         List<News> newsList = newsService.readAllNewsDesc();
         List<NewsDto.Response> responseList = newsMapper.newsListToNewsDtoResponseList(newsList);
         delegateConvertTag(newsList, responseList);
         model.addAttribute("news",responseList);
 
-//        model.addAttribute("news",);
+        if(principal != null)
+            model.addAttribute("userEmail",principal.getName());
+
         return "index"; //머스테치 스타터로인해 반환되는 String 앞뒤로 경로와 확장자가 자동으로 지정됨
     }
 
@@ -52,5 +55,12 @@ public class IndexController {
         for (int i = 0; i < source.size(); i++) {
             newsMapper.newsTagStrToNewsTagArr(destination.get(i), source.get(i));
         }
+    }
+
+
+    @GetMapping(value = "/member/auth/logout")
+    public String logout() {
+
+        return "/";
     }
 }
