6e1d510feaebce2d9da3a861b1006532ccc997e1
JanuaryKim <unool6976@gmail.com>
2023-03-13 20:20:50 +0900

    clean : IndexController 내의 불필요 코드 제거

--- server/src/main/java/my/project/wenews/page/IndexController.java
+++ server/src/main/java/my/project/wenews/page/IndexController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
-import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -68,10 +67,4 @@ public class IndexController {
         }
     }
 
-
-    @GetMapping(value = "/member/auth/logout")
-    public String logout() {
-
-        return "/";
-    }
 }
