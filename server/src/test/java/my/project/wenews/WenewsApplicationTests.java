54c76fe0c15f34a9357f93d14f157c032437126b
JanuaryKim <unool6976@gmail.com>
2023-03-16 23:31:32 +0900

    comment : WenewsApplicationTests에 @SpringBootTest 주석처리

--- server/src/test/java/my/project/wenews/WenewsApplicationTests.java
+++ server/src/test/java/my/project/wenews/WenewsApplicationTests.java
@@ -3,7 +3,7 @@ package my.project.wenews;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WenewsApplicationTests {
 
     @Test
