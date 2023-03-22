ce2460ac23ca38bfddaf3e7d6ff808ade3001d73
JanuaryKim <unool6976@gmail.com>
2023-03-23 00:56:19 +0900

    feat : env.sh 내용 추가

--- server/sh/env.sh
+++ server/sh/env.sh
@@ -1,3 +1,12 @@
 
-echo news_rds_password=${news_rds_password} > .env
+echo news_flyway_url=${news_flyway_url} > .env
+echo news_oauth_google_id=${news_oauth_google_id} >> .env
+echo news_oauth_google_secret=${news_oauth_google_secret} >> .env
+echo news_rds_password=${news_rds_password} >> .env
+echo news_rds_url=${news_rds_url} >> .env
 echo news_rds_username=${news_rds_username} >> .env
+echo news_test_flyway_url=${news_test_flyway_url} >> .env
+echo news_test_rds_url=${news_test_rds_url} >> .env
+
+
+
